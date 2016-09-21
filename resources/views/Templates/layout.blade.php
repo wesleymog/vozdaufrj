c266e7d050459264c9b075e1c8d44416491ff966
Washington Correia <washcpc@terra.com.br>
2016-09-21 17:34:59 -0300

    #inicio projeto menu

---
+++ resources/views/Templates/layout.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Voz Da UFRJ</title>
+	<link rel= "stylesheet" type="text/css" href="drop.css"/>
+</head>
+<body>
+		<div display='inline-block'>	
+				<img src="https://ufrj.br/img/minerva/003minerva_transparente_vertical.png"  width="50px">
+				<h1>Voz Da UFRJ</h1>
+				<form>	
+						Login<input type="text" name="login">
+						<p>Senha<input type="text" name="senha"> 
+						<button>Logar</button></p>						
+				</form>
+		</div>
+
+		
+		        <nav class="navbar navbar-inverse navbar-static-top marginBottom-0" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+              <a class="navbar-brand" href="#" target="_blank">NewWindow</a>
+            </div>
+            
+            <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Active Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Dropdown Link 1</a></li>
+                            <li><a href="#">Dropdown Link 2</a></li>
+                            <li><a href="#">Dropdown Link 3</a></li>
+                            <li class="divider"></li>
+                           
+                        </ul>
+                    </li>
+                    
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+        
+       
+
+     
+</body>
+</html>
