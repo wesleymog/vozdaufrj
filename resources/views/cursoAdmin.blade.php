a3bdda258d939a3eadf102d238cb34db83d15398
luis quirino <luis quirino>
2016-09-29 01:13:22 -0300

    +criado cursoAdmin

---
+++ resources/views/cursoAdmin.blade.php
@@ -0,0 +1,22 @@
+@extends('templates/layout')
+
+
+@section('deletarPredios')
+
+    <button class="btn btn-danger">x</button>
+
+@endsection
+
+@section('botaoPredios')
+
+    <li>Criar novo predio</li>
+
+@endsection
+
+
+@section('editar_curso')
+
+    
+    @yield('conteudo')
+
+@endsection
